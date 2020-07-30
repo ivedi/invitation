84b7257a0b24fe4ae65c00c93eee2f5f0fbc9a4e
ivedi <halil@veriyaz.com.tr>
2020-07-31 02:37:01 +0300

    fixed open close issue

--- asset/main.js
+++ asset/main.js
@@ -3,4 +3,14 @@ const urlParams = new URLSearchParams(queryString);
 const guest = urlParams.has('guest') ? urlParams.get('guest') : '';
 window.onload = () => {
   document.querySelector('.guest').append(guest);
+  document.querySelector('body').addEventListener('click', toggleDetail)
+}
+
+function toggleDetail() {
+  const element = document.querySelector('.card')
+  if (element.classList.contains('open')) {
+    element.classList.remove('open')
+  } else {
+    element.classList.add('open')
+  }
 }
