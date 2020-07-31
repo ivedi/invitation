5bf66ba7bab13d7e1baa1f68d3e389225f4280ee
ivedi <halil@veriyaz.com.tr>
2020-07-31 23:59:20 +0300

    removed iOS for audio playing

--- asset/main.js
+++ asset/main.js
@@ -4,9 +4,10 @@ const guest = urlParams.has('guest') ? urlParams.get('guest') : '';
 window.onload = () => {
   document.querySelector('.guest').append(guest);
 
+  const OS = getOS()
   document.querySelector('.card').addEventListener('click', () => {
     toggleDetail()
-    if (musicLoadState === 'notloaded') {
+    if (OS !== 'iOS' && musicLoadState === 'notloaded') {
       loadMusic()
     }
   })
@@ -66,4 +67,27 @@ function loadMusic() {
       source.start(0);
   });
   xhr.send();
+}
+
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
 }
